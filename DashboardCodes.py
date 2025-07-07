a8219151db8eecf9c4e1f309bfe98469634880fc
DNPeng <komunikasikelembagaan@gmail.com>
2025-07-07 16:10:35 +0700

    Update DashboardCodes.py

--- DashboardCodes.py
+++ DashboardCodes.py
@@ -173,7 +173,7 @@ with col3:
     st.markdown(f"""
     <div class="logo-container">
         <img src="data:image/png;base64,{bps_logo_base64}" alt="BPS Logo" 
-             style="width: 120px; height: 100px; object-fit: contain;">
+             style="width: 200px; height: 100px; object-fit: contain;">
     </div>
     """, unsafe_allow_html=True)
     
